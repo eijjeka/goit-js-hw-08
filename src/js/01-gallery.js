644255c9b53ebc66bdacae2cd4be0a9b6d60a4d8
eijjeka <eijjeka@gmail.com>
2022-01-12 16:33:40 +0200

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const parentEl = document.querySelector('.gallery');
+
+// const markup = galleryItems.map(({ preview, original, description }) =>
+// `<div class="gallery__item">
+// <a class="gallery__link" href="${original}"
+// onclick="event.preventDefault()">
+// <img
+// class="gallery__image"
+// src="${preview}"
+// data-source="${original}"
+// alt="${description}"
+// />
+// </a>
+// </div>`).join('');
+
+// parentEl.insertAdjacentHTML('beforeend', markup);
+
+
+// function onImgContainerClick(event) {
+//     if (!event.target.classList.contains("gallery__image")) {
+//         return;
+//     }
+//     const instance = basicLightbox.create(
+//         `
+//         <img src="${event.target.dataset.source}" width="800" height="600">
+//         `,
+//         {
+//             onShow: (instance) => {
+//                 window.addEventListener("keydown", onClickEscape);
+//             },
+//             onClose: (instance) =>
+//             window.removeEventListener("keydown", onClickEscape),
+//         }
+//         );
+//         const onClickEscape = (event) => {
+//             if (event.key === "Escape") {
+//                 instance.close();
+//             }
+//         };
+//         instance.show();
+//     }
+    
+//     parentEl.addEventListener('click', onImgContainerClick);
