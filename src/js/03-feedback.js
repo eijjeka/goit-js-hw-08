0c817ca354b3276b16180d003dc78c59277dbba0
eijjeka <eijjeka@gmail.com>
2022-01-15 20:31:45 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,42 @@
-const form = document.querySelector('.feedback-form');
-console.log(form)
+import throttle from 'lodash.throttle'
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
 const STORAGE_KEY = "feedback-form-state";
 
 const formData = {};
 
-form.addEventListener('input', event => {
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+populateTextForm();
+
+function onFormInput(event) {
     formData[event.target.name] = event.target.value;
-    console.log(formData)
-});
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(`E-mail: ${formData.email}`)
+    console.log(`Message: ${formData.message}`)
+};
+
+function populateTextForm() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY)
+    if (saveMessage) {
+        const outputText = JSON.parse(saveMessage);
+        refs.email.value = outputText.email;
+        refs.textarea.value = outputText.message;
+    }
+};
+
+
+
 
 
