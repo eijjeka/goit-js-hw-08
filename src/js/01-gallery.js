c0d9a5a17134bf2299a1fa048208ce7c86974b21
eijjeka <eijjeka@gmail.com>
2022-01-17 12:46:38 +0200

    update task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,12 @@
+
+import cardImage from '../templates/image-card.hbs';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
-
-
-console.log(galleryItems);
-
 const parentEl = document.querySelector('.gallery');
 
-const markup = galleryItems.map(({ preview, original, description }) =>
-`<a class="gallery__link" href="${original}"
-onclick="event.preventDefault()">
-<img
-class="gallery__image"
-src="${preview}"
-data-source="${original}"
-alt="${description}"
-/>
-</a>`).join('');
-
-parentEl.insertAdjacentHTML('beforeend', markup);
+parentEl.insertAdjacentHTML('beforeend', cardImage(galleryItems));
 
 new SimpleLightbox('.gallery a', {
     captions: true,
@@ -28,11 +15,3 @@ new SimpleLightbox('.gallery a', {
     captionPosition: 'bottom',
     captionDelay: 250,
 });
-
-// new SimpleLightbox('.gallery a', { 
-//     captions: true,
-//     captionSelector: 'img',
-//     captionsData: 'alt',
-//     captionPosition: 'bottom',
-//     captionDelay: 250,
-// });
